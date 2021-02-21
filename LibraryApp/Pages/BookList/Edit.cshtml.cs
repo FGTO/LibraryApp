03fbb274734815d77b8d311e435d7a67d54a9ddf
FGTO <fongqto@gmail.com>
2021-02-21 16:41:35 +0100

    Add edit function for database entry

---
+++ LibraryApp/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryApp.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibraryApp.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private LibraryDbContext _db;
+
+        public EditModel(LibraryDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+        }
+    }
+}
